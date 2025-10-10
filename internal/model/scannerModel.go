7a0710264bbdeecae4b6e13274b5deb2eca5bc92
Deswandy <deswandywong23@gmail.com>
2025-10-10 23:41:04 +0700

    feat: supplementary model

--- internal/model/scannerModel.go
+++ internal/model/scannerModel.go
@@ -12,4 +12,10 @@ type Scanner struct {
 	Organization   Organization  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
-
+type PendingRFID struct {
+	gorm.Model
+	RFID       string    `gorm:"uniqueIndex;not null" json:"rfid"`
+	ScannerID  uint      `json:"scannerId"`
+	Scanner    Scanner   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"scanner"`
+	Registered bool      `gorm:"default:false" json:"registered"`
+}
